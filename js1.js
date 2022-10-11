8dc2c4cfbae3ae13917728e30329a0566810483b
nkhl-10 <108462845+nkhl-10@users.noreply.github.com>
2022-10-11 12:13:23 +0530

    Add files via upload

---
+++ js1.js
@@ -0,0 +1,31 @@
+
+// function
+
+// alert("hello");
+
+// var mame;
+// name="hi";
+// alert(name);
+
+
+// confirm("are you sure");
+// prompt("please enter your feedback");
+
+// coment in console
+// console.log("hi bro");
+//write in document
+// document.write("hello ");
+
+
+// var n="nikhil";
+// document.write(n);
+
+var x=document.getElementById("main".innerHTML="<h1>hello<h1/>")
+console.log(x);
+
+document.getElementsByClassName();
+innerHTML();
+
+
+
+
